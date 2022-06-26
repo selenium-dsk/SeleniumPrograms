307e4bf9c01ee1498872254c869ad1f743476c16
sujand <sujand@localhost>
2022-06-26 07:47:22 +0530

    Third Commit

--- GithubDemo/src/test/java/automationFramework/SampleProgram.java
+++ GithubDemo/src/test/java/automationFramework/SampleProgram.java
@@ -4,7 +4,7 @@ public class SampleProgram {
 
 	public static void main(String[] args) {
 		System.out.println("Today is Sunday");
-		System.out.println("Tomorrow is Monday");
+		System.out.println("Tomorrow is Monday.");
 	}
 
 }
